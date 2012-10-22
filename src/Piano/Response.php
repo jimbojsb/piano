9d84abb96d06e63ba49bf9d30f1ac7abfdc61d0e
Josh Butts <josh@joshbutts.com>
2012-10-22 02:08:06 -0500

    add response redirection

--- src/Piano/Response.php
+++ src/Piano/Response.php
@@ -4,7 +4,7 @@ namespace Piano;
 class Response
 {
     protected $statusCode = 200;
-    protected $headers;
+    protected $headers = array();
     protected $body ;
 
     public function __construct($body = '')
@@ -26,9 +26,24 @@ class Response
         return $this;
     }
 
+    public function addHeader($headerName, $headerValue)
+    {
+        $this->headers[$headerName] = $headerValue;
+    }
+
     public function __toString()
     {
+        foreach ($this->headers as $headerName => $headerValue) {
+            header("$headerName: $headerValue");
+        }
         http_response_code($this->statusCode);
         return $this->body;
+    }
+
+    public function redirect($location, $statusCode = 301)
+    {
+        $this->addHeader('Location', $location);
+        $this->setStatusCode($statusCode);
+        return $this;
     }
 }
